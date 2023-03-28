45393a915d7888991e2a03f43645a927cbe12225
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-28 14:39:28 +0200

    sequencer: store_read implementation and endpoint

--- sequencer/src/kernel.rs
+++ sequencer/src/kernel.rs
@@ -15,6 +15,7 @@ mod external_kernel {
     use tezos_smart_rollup_host::{
         path::RefPath,
         runtime::{Runtime, RuntimeError},
+        Error,
     };
 
     const COUNTER_PATH: RefPath = RefPath::assert_from(b"/counter");
@@ -24,12 +25,32 @@ mod external_kernel {
         inner: u64,
     }
 
+    impl Counter {
+        fn increment(self) -> Self {
+            Self {
+                inner: self.inner + 1,
+            }
+        }
+    }
+
     pub fn write_counter<Host: Runtime>(
         host: &mut Host,
         counter: &Counter,
     ) -> Result<(), RuntimeError> {
         let src = counter.inner.to_be_bytes();
         host.store_write(&COUNTER_PATH, &src, 0)
+    }
+
+    pub fn read_counter<Host: Runtime>(host: &mut Host) -> Result<Option<Counter>, RuntimeError> {
+        match host.store_read(&COUNTER_PATH, 0, 8) {
+            Err(RuntimeError::HostErr(Error::StoreNotANode)) => Ok(None),
+            Err(err) => Err(err),
+            Ok(bytes) => {
+                let bytes = bytes.try_into().unwrap();
+                let inner = u64::from_be_bytes(bytes);
+                Ok(Some(Counter { inner }))
+            }
+        }
     }
 
     pub fn entry<Host: Runtime>(host: &mut Host) {
@@ -41,8 +62,8 @@ mod external_kernel {
             match input {
                 Ok(Some(_)) => {
                     host.write_debug("A message has been received");
-
-                    let counter = Counter::default();
+                    let counter = read_counter(host).unwrap().unwrap_or_default();
+                    let counter = counter.increment();
                     let () = write_counter(host, &counter).unwrap();
                 }
                 Ok(None) => {
