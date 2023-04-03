14cde0b8d09b87c5872090aa49ad45ee0bebeb40
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-03 12:06:32 +0200

    sequencer: add tezos listener

---
+++ sequencer/src/tezos_listener.rs
@@ -0,0 +1,33 @@
+use crate::node::{QueueContent, QueueMsg, TezosHeader};
+use futures_util::StreamExt;
+use tokio::sync::mpsc::Sender;
+
+pub struct TezosListener {}
+
+impl TezosListener {
+    pub fn listen(sender: Sender<QueueMsg>) {
+        tokio::spawn(async move {
+            let mut stream = reqwest::get("https://mainnet.tezos.marigold.dev/monitor/heads/main")
+                .await
+                .unwrap()
+                .bytes_stream();
+
+            while let Some(Ok(item)) = stream.next().await {
+                let bytes = item.to_vec();
+
+                let header = serde_json::from_slice::<TezosHeader>(&bytes);
+                match header {
+                    Err(_) => {}
+                    Ok(header) => {
+                        let msg = QueueMsg {
+                            promise: None,
+                            content: QueueContent::TezosHeader(header),
+                        };
+
+                        let _ = sender.send(msg).await;
+                    }
+                }
+            }
+        });
+    }
+}
