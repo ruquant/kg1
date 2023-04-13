fb248b54d94a4f1afa1ae0aa6938781a3b2c3784
Victor Dumitrescu <v.dumitrescu@outlook.com>
2023-04-13 18:12:14 +0200

    Simple kernels: outbox messages

---
+++ 04_outbox_kernel/src/lib.rs
@@ -0,0 +1,122 @@
+use tezos_crypto_rs::hash::SmartRollupHash;
+use tezos_smart_rollup_encoding::{
+    contract::Contract,
+    entrypoint::Entrypoint,
+    inbox::{InboxMessage, InternalInboxMessage},
+    michelson::{Michelson, MichelsonInt},
+    outbox::{OutboxMessage, OutboxMessageTransaction, OutboxMessageTransactionBatch},
+};
+use tezos_smart_rollup_entrypoint::kernel_entry;
+use tezos_smart_rollup_host::runtime::Runtime;
+
+use tezos_data_encoding::enc::BinWriter;
+
+// Read inbox messages, only looking at internal transfer messages directed to
+// this kernel's address. For each such message, write an outbox message
+// addressed to a predetermined L1 contract containing the same Michelson
+// payload as the inbox message.
+fn read_inbox_message<Expr: Michelson>(host: &mut impl Runtime, own_address: &SmartRollupHash) {
+    let input = host.read_input();
+    match input {
+        Err(_) | Ok(None) => (),
+        Ok(Some(message)) => {
+            // Parse the payload of the message
+            let parsed_msg = InboxMessage::<Expr>::parse(message.as_ref()).unwrap();
+            match parsed_msg {
+                (remaining, InboxMessage::Internal(msg)) => {
+                    assert!(remaining.is_empty());
+                    match msg {
+                        InternalInboxMessage::Transfer(m) => {
+                            if m.destination.hash() == own_address {
+                                host.write_debug("Internal message: transfer for me\n");
+                                write_outbox_message(host, m.payload);
+                            } else {
+                                host.write_debug("Internal message: transfer not for me\n")
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                _ => (),
+            }
+            read_inbox_message::<MichelsonInt>(host, &own_address);
+        }
+    }
+}
+
+const L1_CONTRACT_ADDRESS: &str = "KT1RycYvM4EVs6BAXWEsGXaAaRqiMP53KT4w";
+const L1_CONTRACT_ENTRYPOINT: &str = "entry";
+
+fn write_outbox_message<Expr: Michelson>(host: &mut impl Runtime, payload: Expr) {
+    let destination = Contract::from_b58check(L1_CONTRACT_ADDRESS).unwrap();
+    let entrypoint = Entrypoint::try_from(L1_CONTRACT_ENTRYPOINT.to_string()).unwrap();
+    let transaction = OutboxMessageTransaction {
+        parameters: payload,
+        destination,
+        entrypoint,
+    };
+    let batch = OutboxMessageTransactionBatch::from(vec![transaction]);
+    let message = OutboxMessage::AtomicTransactionBatch(batch);
+    let mut output = Vec::default();
+    message.bin_write(&mut output).unwrap();
+    host.write_output(&output).unwrap();
+}
+
+fn entry(host: &mut impl Runtime) {
+    let own_address = host.reveal_metadata().unwrap().address().unwrap();
+    read_inbox_message::<MichelsonInt>(host, &own_address);
+    host.mark_for_reboot().unwrap();
+}
+
+kernel_entry!(entry);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use tezos_crypto_rs::hash::HashType::ContractKt1Hash;
+    use tezos_data_encoding::nom::NomReader;
+    use tezos_smart_rollup_encoding::public_key_hash::PublicKeyHash;
+    use tezos_smart_rollup_mock::{MockHost, TransferMetadata};
+
+    use tezos_smart_rollup_encoding::smart_rollup::SmartRollupAddress;
+
+    const SENDER: &str = "KT1EfTusMLoeCAAGd9MZJn5yKzFr6kJU5U91";
+    const SOURCE: &str = "tz1SodoUsWVe1Yey9eMFbqRUtNpBWfir5NRr";
+    const OTHER_ADDR: &str = "sr1RYurGZtN8KNSpkMcCt9CgWeUaNkzsAfXf";
+
+    // Check that when the inbox contains a transfer message addressed to
+    // this rollup an outbox message with the same payload will be written
+    #[test]
+    fn transfer_outbox() {
+        let mut host = MockHost::default();
+
+        let sender = ContractKt1Hash.b58check_to_hash(SENDER).unwrap();
+        let source = PublicKeyHash::from_b58check(SOURCE).unwrap();
+        let metadata = TransferMetadata::new(sender, source);
+        let payload = MichelsonInt::from(32);
+        host.add_transfer(payload, &metadata);
+        entry(&mut host);
+        let (_, f) = OutboxMessageTransaction::<MichelsonInt>::nom_read(
+            &host.outbox_at(host.level())[0].as_slice()[5..],
+        )
+        .unwrap();
+        assert!(f.parameters == MichelsonInt::from(32));
+    }
+
+    #[test]
+    // Check that when the inbox only contains a transfer message addressed to
+    // a different rollup no outbox message is written
+    fn transfer_ignore() {
+        let mut host = MockHost::default();
+
+        let sender = ContractKt1Hash.b58check_to_hash(SENDER).unwrap();
+        let source = PublicKeyHash::from_b58check(SOURCE).unwrap();
+        let mut metadata = TransferMetadata::new(sender, source);
+        let destination = SmartRollupAddress::from_b58check(OTHER_ADDR).unwrap();
+        metadata.override_destination(destination);
+        let payload = MichelsonInt::from(32);
+        host.add_transfer(payload, &metadata);
+        entry(&mut host);
+        assert!(host.outbox_at(host.level()).is_empty());
+    }
+}
