affb414b9ceca69f3c7835368dd9093130045384
lykimq <lykimq@gmail.com>
2023-06-07 16:08:30 +0200

    Dungeon: add actor player

---
+++ 10_dungeon_app/kernel/src/player.rs
@@ -0,0 +1,15 @@
+use crate::item::Item;
+
+pub const MAX_ITEMS: usize = 2;
+
+#[derive(Clone, PartialEq)]
+pub struct Player {
+    pub x_pos: usize,
+    pub y_pos: usize,
+}
+
+impl Player {
+    pub fn new(x_pos: usize, y_pos: usize) -> Self {
+        Self { x_pos, y_pos }
+    }
+}
